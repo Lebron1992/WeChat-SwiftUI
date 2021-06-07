8545babf0d11c9be0ec5250f4b04f5455dee8383
Lebron1992 <wenzhi.zeng@outlook.com>
2021-06-07 17:58:40 +0800

    Initial Commit

---
+++ WeChat-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeChat-SwiftUI
+//
+//  Created by Lebron on 2021/6/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeChat-SwiftUI/WeChat_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeChat_SwiftUIApp.swift
+//  WeChat-SwiftUI
+//
+//  Created by Lebron on 2021/6/7.
+//
+
+import SwiftUI
+
+@main
+struct WeChat_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
