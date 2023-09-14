f46b58e2651c437c6ea85fdba63bcb8c8137efea
Lebron1992 <wenzhi.zeng@outlook.com>
2023-09-14 15:59:07 +0800

    update app reducer

--- WeChat-SwiftUI/ComposableArchitecture/App.swift
+++ WeChat-SwiftUI/ComposableArchitecture/App.swift
@@ -8,26 +8,47 @@ enum AppAction: Equatable {
   case system(SystemAction)
 }
 
-let appReducer: some ReducerProtocol<AppState, AppAction> = CombineReducers {
-  Scope(state: \AppState.authState, action: /AppAction.auth) {
-    AuthReducer()
+struct AppReducer: ReducerProtocol {
+  var body: some ReducerProtocol<AppState, AppAction> {
+    authScope
+    chatsScope
+    contactsScope
+    rootScope
+    systemScope
+    CommonReducer()
   }
-  Scope(state: \AppState.chatsState, action: /AppAction.chats) {
-    ChatsReducer()
-  }
-  Scope(state: \AppState.contactsState, action: /AppAction.contacts) {
-    ContactsReducer()
-  }
-  Scope(state: \AppState.rootState, action: /AppAction.root) {
-    RootReducer()
-  }
-  Scope(state: \AppState.systemState, action: /AppAction.system) {
-    SystemReducer()
-  }
-  CommonReducer()
 }
 
-struct CommonReducer: ReducerProtocol {
+let appReducer = AppReducer()
+
+// NOTE: 初始化 `Scope`，一定要先定义变量 `reducer` 再返回，才会编译通过。（可能编译器有问题）
+
+private let authScope = Scope(state: \AppState.authState, action: /AppAction.auth) {
+  let reducer = AuthReducer()
+  return reducer
+}
+
+private let chatsScope = Scope(state: \AppState.chatsState, action: /AppAction.chats) {
+  let reducer = ChatsReducer()
+  return reducer
+}
+
+private let contactsScope = Scope(state: \AppState.contactsState, action: /AppAction.contacts) {
+  let reducer = ContactsReducer()
+  return reducer
+}
+
+private let rootScope = Scope(state: \AppState.rootState, action: /AppAction.root) {
+  let reducer = RootReducer()
+  return reducer
+}
+
+private let systemScope = Scope(state: \AppState.systemState, action: /AppAction.system) {
+  let reducer = SystemReducer()
+  return reducer
+}
+
+private struct CommonReducer: ReducerProtocol {
   func reduce(into state: inout AppState, action: AppAction) -> EffectTask<AppAction> {
     switch action {
       // chats
